f8e6d74c0cc5087cdac412fd6ab8a010c9afa48d
Xiaoxiong Xing <xingxiaoxiong@frontier.hokudai.ac.jp>
2014-11-02 15:05:40 +0900

    Add user name filed

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141102031501) do
+ActiveRecord::Schema.define(version: 20141102054641) do
 
   create_table "pins", force: true do |t|
     t.string   "description"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 20141102031501) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
